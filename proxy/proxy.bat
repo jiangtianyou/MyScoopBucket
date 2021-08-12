15f7d6ce1a0b76e2da2ea7904ff27fbc716bb5c4
jiangtianyou <jiangtianyou@wdit.com.cn>
2021-08-12 10:42:53 +0800

    2021/08/12 周四10:42:53.33

--- proxy/proxy.bat
+++ proxy/proxy.bat
@@ -1,2 +1,18 @@
+@echo off
+rem 需要使用^对|进行escape,否则会直接被当做for语法内容解析
+set v2rayon=0
+for /F "delims=" %%i in ('ps -ef ^| grep v2rayN ^| wc -l') DO (
+	set v2rayon=%%i
+)
+if NOT %v2rayon%==1  (
+	echo v2ray is not running,now open v2rayN
+	start /B v2rayn
+)
+
+rem set proxy environment,usually supported by net tools
+
 set https_proxy=localhost:10809
 set http_proxy=localhost:10809
+
+echo set https_proxy=%https_proxy%
+echo set http_proxy=%http_proxy%
