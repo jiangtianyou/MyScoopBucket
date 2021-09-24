7cd20e37bf9934324ddc01b29f118108ab106cc6
jiangtianyou <jiangtianyou@gmm01.com>
2021-09-25 01:15:08 +0800

    2021/09/25 周六 1:15:08.56

--- bins/jk.bat
+++ bins/jk.bat
@@ -1,16 +1,11 @@
 @echo off
-
 set tmpfile=tmp-%RANDOM%.txt
 jps | grep -v Jps | awk "{print $1}">%tmpfile%
-
 FOR /F %%i IN (%tmpfile%) DO (
 	taskkill /F /PID %%i
 )
-
 del %tmpfile%
-
 @echo on
-
 echo "Success remote all java process！" 
 
 
